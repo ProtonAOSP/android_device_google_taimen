a57493c0ea778b9dd13c1cf21c8573fcb7cff75c
Danny Lin <danny@kdrag0n.dev>
2020-09-20 00:43:20 -0700

    taimen: Don't restrict vendor file owner modules
    Our custom boot animation and fonts are copied from a vendor module.
    
    Change-Id: I74a6478030c1fd57f67256e1577c222d06e8a821

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -33,7 +33,7 @@ PRODUCT_COPY_FILES += \
     device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
 
-PRODUCT_RESTRICT_VENDOR_FILES := owner
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 
